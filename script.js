0276d78cba1c15c79f24d29e34b137158e6a5527
Kevin Volkel <kevinator900@gmail.com>
2020-11-23 13:51:15 -0700

    added styling and some functionality

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 "use strict";
+
+let turn = 0;
+let xWins = 0;
+let oWins = 0;
+
+document.getElementById("player").textContent = `Player: X`;
+document.getElementById("turn").textContent = `Turn: 0`;
+
+let markBox = function (box){
+    if(turn % 2 == 0){
+        document.getElementById(box).textContent = "X";
+    }else{
+        document.getElementById(box).textContent = "O";
+    }
+
+    // checkWin();
+
+    turn++;
+    if(turn % 2 == 0){
+        document.getElementById("player").textContent = `Player: X`;
+    }else{
+        document.getElementById("player").textContent = `Player: O`;
+    }
+    
+    document.getElementById("turn").textContent = `Turn: ${turn}`;
+}
